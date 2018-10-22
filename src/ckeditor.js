e75404d2a5f8865dbe4a2df1cfe2d8d8b3ec9f2d
Komthep Saksriyuttana <komthep@gmail.com>
2018-10-22 15:18:43 +0700

    update to latest

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,61 +4,44 @@
  */
 
 // The editor creator to use.
-import InlineEditoBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor'
-
+import InlineEditorBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor'
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter'
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat'
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage'
-import Heading from '@ckeditor/ckeditor5-heading/src/heading'
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough'
 import Image from '@ckeditor/ckeditor5-image/src/image'
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption'
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle'
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar'
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload'
 import Link from '@ckeditor/ckeditor5-link/src/link'
-import List from '@ckeditor/ckeditor5-list/src/list'
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed'
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
-import Table from '@ckeditor/ckeditor5-table/src/table'
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar'
 
-export default class ClassicEditor extends InlineEditoBase {}
+export default class SabuyEditor extends InlineEditorBase {}
 
 // Plugins to include in the build.
-ClassicEditor.builtinPlugins = [
+SabuyEditor.builtinPlugins = [
   Essentials,
-  UploadAdapter,
   Autoformat,
   Bold,
   Italic,
-  BlockQuote,
-  EasyImage,
-  Heading,
+  Strikethrough,
   Image,
   ImageCaption,
   ImageStyle,
   ImageToolbar,
-  ImageUpload,
   Link,
-  List,
-  MediaEmbed,
-  Paragraph,
-  Table,
-  TableToolbar
+  Paragraph
 ]
 
 // Editor configuration.
-ClassicEditor.defaultConfig = {
+SabuyEditor.defaultConfig = {
   toolbar: {
     items: [
-      'heading',
-      '|',
       'bold',
       'italic',
+      'strikethrough',
+      '|',
       'link',
       'undo',
       'redo'
@@ -70,13 +53,6 @@ ClassicEditor.defaultConfig = {
       'imageStyle:side',
       '|',
       'imageTextAlternative'
-    ]
-  },
-  table: {
-    contentToolbar: [
-      'tableColumn',
-      'tableRow',
-      'mergeTableCells'
     ]
   },
   // This value must be kept in sync with the language defined in webpack.config.js.
